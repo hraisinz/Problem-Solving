e9187c31ec8f392d37b5d59da9c4e4b9cbfeadeb
hraisinz <hraisinz@gmail.com>
2019-08-22 23:56:56 +0530

    Refactoring

--- src/org/raisinz/Main.java
+++ src/org/raisinz/Main.java
@@ -6,18 +6,18 @@ public class Main {
     public static void main(String[] args) {
 
         int[] sortedArr={10,20,30,30,44,44,55,60,60,70};
-        int[] arr=new int[10];
+        int[] resultArr=new int[10];
         int j=0;
         for (int i = 0; i <sortedArr.length-1 ; i++) {
            if(sortedArr[i] != sortedArr[i+1]){
-                arr[j]=sortedArr[i];
+                resultArr[j]=sortedArr[i];
                 j++;
                 count++;
            }
         }
-        arr[j]=sortedArr[sortedArr.length-1];
+        resultArr[j]=sortedArr[sortedArr.length-1];
         for (int i = 0; i <=count ; i++) {
-            System.out.println(arr[i]);
+            System.out.println(resultArr[i]);
         }
     }
 }
