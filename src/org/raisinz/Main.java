4245496d536f802328167511353541a5a0d3d831
hraisinz <hraisinz@gmail.com>
2019-08-23 10:48:37 +0530

    Removing duplicate elements from sorted array using extra space

--- src/org/raisinz/Main.java
+++ src/org/raisinz/Main.java
@@ -2,22 +2,27 @@ package org.raisinz;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        int[] sortedArr={10,20,30,30,44,44,55,60,60,70};
-        int[] resultArr=new int[10];
+    public void removeDuplicates(int[] sortedArr){
+        int[] resultArr=new int[sortedArr.length];
         int j=0;
         for (int i = 0; i <sortedArr.length-1 ; i++) {
-           if(sortedArr[i] != sortedArr[i+1]){
+            if(sortedArr[i] != sortedArr[i+1]){
                 resultArr[j]=sortedArr[i];
                 j++;
-           }
+            }
         }
         resultArr[j]=sortedArr[sortedArr.length-1];
         for (int i = 0; i <=j ; i++) {
             System.out.println(resultArr[i]);
         }
     }
+
+    public static void main(String[] args) {
+
+        int[] sortedArr={10,20,30,30,44,44,55,60,60,70,70,70,80};
+        Main main = new Main();
+        main.removeDuplicates(sortedArr);
+        }
 }
 
 
