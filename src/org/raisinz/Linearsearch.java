3bf68e3bec96c324badf4ee48de6f00d12379f68
hraisinz <hraisinz@gmail.com>
2019-08-23 17:52:56 +0530

    Searching an element from array using Linear Search.

---
+++ src/org/raisinz/Linearsearch.java
@@ -0,0 +1,25 @@
+package org.raisinz;
+
+public class Linearsearch {
+
+    public void searchElement(int[] values,int searchValue){
+
+        boolean found=false;
+        for (int i = 0; i < values.length; i++) {
+            if(values[i]== searchValue){
+                found=true;
+                break;
+            }
+        }
+        if(found) System.out.println("Element Found");
+        else System.out.println("Element Not Found");
+    }
+
+    public static void main(String[] args) {
+
+        int[] values={50,25,75,100,42,64};
+        Linearsearch linearsearch = new Linearsearch();
+        linearsearch.searchElement(values,100);
+
+    }
+}
