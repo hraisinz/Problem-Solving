1abf04ffcd95fcab917a9ec1c032ce164221eda0
hraisinz <hraisinz@gmail.com>
2019-08-23 11:32:21 +0530

    Removing the duplicate elements from the sorted array using Constant Space

---
+++ src/org/raisinz/Removeduplicates.java
@@ -0,0 +1,37 @@
+package org.raisinz;
+
+public class Removeduplicates {
+
+    private int currentIndex;
+    private int[] resultArr;
+
+    public void removeDuplicate(int[] sortedArr){
+        int size=sortedArr.length-1;
+        int current=0;
+        for (int i = 0; i <size ; i++) {
+            if(sortedArr[i] != sortedArr[i+1]){
+                sortedArr[current]=sortedArr[i];
+                current++;
+            }
+        }
+        sortedArr[current]=sortedArr[size];
+        currentIndex=current;
+        resultArr=sortedArr;
+    }
+
+    public void getArray(){
+        for (int i = 0; i <resultArr.length; i++) {
+            System.out.println(resultArr[i]);
+        }
+    }
+    public static void main(String[] args) {
+
+        int[] sortedArr={10,20,30,30,44,44,55,60,60,70,70,90};
+        Removeduplicates removeduplicates = new Removeduplicates();
+        removeduplicates.removeDuplicate(sortedArr);
+        removeduplicates.getArray();
+
+
+    }
+}
+
