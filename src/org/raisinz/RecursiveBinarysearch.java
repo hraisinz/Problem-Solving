70921441298a3474793f5e94b3f0a35cf5d55d81
hraisinz <hraisinz@gmail.com>
2019-08-24 11:17:05 +0530

    Recursive Binary Search

---
+++ src/org/raisinz/RecursiveBinarysearch.java
@@ -0,0 +1,26 @@
+package org.raisinz;
+
+public class RecursiveBinarysearch {
+
+    public int binarySearch(int[] values,int intial,int last,int element){
+
+        int middle=(intial+last)/2;
+        if(intial>last) return -1;
+        if(element == values[middle]) return middle;
+        else if(element < values[middle]) return binarySearch(values,intial,middle-1,element);
+        else return binarySearch(values,middle+1,last,element);
+    }
+
+    public static void main(String[] args) {
+        int values[] = {10,20,30,40,50,60};
+        int element = 50;
+        int intial=0;
+        int last=values.length-1;
+
+        RecursiveBinarysearch recursiveBinarysearch = new RecursiveBinarysearch();
+        int found = recursiveBinarysearch.binarySearch(values, intial, last, element);
+        if(found == -1) System.out.println("Element Not Found");
+        else System.out.println("Element Found at Index :"+found);
+
+    }
+}
