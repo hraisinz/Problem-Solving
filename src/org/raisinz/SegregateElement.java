e5be443e4b713c446c3ef6b5c70a687c28b23014
hraisinz <hraisinz@gmail.com>
2019-08-24 17:20:42 +0530

    Segregate 0s on leftside and 1s on rightside

---
+++ src/org/raisinz/SegregateElement.java
@@ -0,0 +1,35 @@
+package org.raisinz;
+
+public class SegregateElement {
+
+
+    public void segregateValue(int[]  arr){
+        int size=arr.length-1;
+        int left=0;
+        int right=size;
+        while(left < right){
+            while (arr[left] == 0 && left<right) left++;
+            while (arr[right] == 1 && left<right) right--;
+            if(left < right)
+            {
+                arr[left]=0;
+                arr[right]=1;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public void getElement(int[] values){
+        for (int i = 0; i <values.length ; i++) {
+            System.out.println(values[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int array[] = { 0, 1, 0, 1, 1, 0 };
+        SegregateElement segregateElement = new SegregateElement();
+        segregateElement.segregateValue(array);
+        segregateElement.getElement(array);
+    }
+}
