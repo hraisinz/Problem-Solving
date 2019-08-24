e1d7ce662a3e8fe9c00e2004e2f47005d9b56650
hraisinz <hraisinz@gmail.com>
2019-08-24 12:27:24 +0530

    Count Occurences of element in sorted array with duplicates.

---
+++ src/org/raisinz/Countoccurrence.java
@@ -0,0 +1,47 @@
+package org.raisinz;
+
+public class Countoccurrence {
+
+    private final int divValue;
+    private final int elementValue;
+
+    public Countoccurrence(){
+        divValue=2;
+        elementValue=0;
+    }
+
+    public int binarySearch(int[] values,int element,boolean search){
+        int intial=0;
+        int last=values.length-1;
+        int res= -1;
+        while (intial <= last){
+            int middle=(intial+last)/divValue;
+            if(values[middle] == element) {
+                res=middle;
+                if(search) last=middle-1;
+                else intial=middle+1;
+            }
+            else if(element < values[middle]) last=middle-1;
+            else intial=middle+1;
+        }
+        return res;
+    }
+
+    public void countOccurenceOfElements(int[] values,int element){
+
+        int intialIndex = binarySearch(values, element, true);
+        if(intialIndex == -1) System.out.println("Count of "+element+" is : "+elementValue);
+        else{
+            int lastIndex = binarySearch(values, element, false);
+            System.out.println("count of "+element+" is : "+(lastIndex-intialIndex+1));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int []sortedArr={10,10,20,20,30,30,30,30,40,50,50};
+        Countoccurrence countoccurrence = new Countoccurrence();
+        int element= 10;
+        countoccurrence.countOccurenceOfElements(sortedArr,element);
+    }
+}
