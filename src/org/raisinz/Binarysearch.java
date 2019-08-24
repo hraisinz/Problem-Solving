444be788a569bbdbb25ec3ed0ec2fad5a4919a37
hraisinz <hraisinz@gmail.com>
2019-08-24 11:26:05 +0530

    Refactoring Binary Search

--- src/org/raisinz/Binarysearch.java
+++ src/org/raisinz/Binarysearch.java
@@ -8,22 +8,22 @@ public class Binarysearch {
         divValue=2;
     }
 
-    public void binarySearch(int[] values,int element){
+    public int binarySearch(int[] values,int element){
         int intial=0;
         int last=values.length-1;
         int middle=(intial+last)/divValue;
         while (intial <= last){
-            if(values[middle] == element) {
-                System.out.println("Element Found at Index : " + middle);
-                break;
-            }
+            if(values[middle] == element) return middle;
             else if(element < values[middle]) last=middle-1;
             else intial=middle+1;
             middle=(intial+last)/divValue;
         }
-        if(intial > last){
-            System.out.println("Element Not Found");
-        }
+        return -1;
+    }
+
+    public void elementFound(int found){
+        if(found == -1) System.out.println("Element Not Found");
+        else System.out.println("Element Found at Index : "+found);
     }
 
     public static void main(String[] args) {
@@ -31,7 +31,7 @@ public class Binarysearch {
         int element1=35;
         int element2=32;
         Binarysearch binarysearch = new Binarysearch();
-        binarysearch.binarySearch(values,element1);
-        binarysearch.binarySearch(values,element2);
+        binarysearch.elementFound(binarysearch.binarySearch(values,element1));
+        binarysearch.elementFound(binarysearch.binarySearch(values,element2));
     }
 }
