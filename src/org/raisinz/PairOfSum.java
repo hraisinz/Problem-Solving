b579b33dfc108b8883f196a9fee68accea25b4a6
hraisinz <hraisinz@gmail.com>
2019-08-25 10:14:29 +0530

    Finding the pairs of given sum in sorted array

---
+++ src/org/raisinz/PairOfSum.java
@@ -0,0 +1,24 @@
+package org.raisinz;
+
+public class PairOfSum {
+
+    public void findPair(int[]  values,int sum){
+        int left=0;
+        int right=values.length-1;
+        while (left<right){
+            if((values[left]+values[right]) > sum) right--;
+            else if((values[left]+values[right]) < sum) left++;
+            else{
+                System.out.println("Pair is : "+values[left]+","+values[right]);
+                left++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        int[] arr={1,2,3,4,5,6,7,8,9,10};
+        int sum=8;
+        PairOfSum pairOfSum = new PairOfSum();
+        pairOfSum.findPair(arr,sum);
+    }
+}
