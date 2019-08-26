a27598f2bf40a81bfb0ebee77b5d934353fcac6c
hraisinz <hraisinz@gmail.com>
2019-08-26 21:31:35 +0530

    Fibonacci series with recursion

---
+++ src/org/raisinz/FibonacciRecursion.java
@@ -0,0 +1,15 @@
+package org.raisinz;
+
+public class FibonacciRecursion {
+
+    public int fibRecursion(int n){
+
+        if(n <= 1) return n;
+        return fibRecursion(n-1) + fibRecursion(n-2);
+    }
+    public static void main(String[] args) {
+
+        FibonacciRecursion fibonacciRecursion = new FibonacciRecursion();
+        System.out.println(fibonacciRecursion.fibRecursion(10));
+    }
+}
