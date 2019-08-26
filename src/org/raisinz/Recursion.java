34e1de5c0226a49f1b0f3b3dda5f24cd2105ca1a
hraisinz <hraisinz@gmail.com>
2019-08-26 20:16:24 +0530

    Recursion

---
+++ src/org/raisinz/Recursion.java
@@ -0,0 +1,15 @@
+package org.raisinz;
+
+public class Recursion {
+
+    public int getFactorial(int num){
+        if(num == 0) return 1;
+        return (num * getFactorial(num-1));
+    }
+
+    public static void main(String[] args) {
+
+        Recursion recursion = new Recursion();
+        System.out.println(recursion.getFactorial(6));
+    }
+}
