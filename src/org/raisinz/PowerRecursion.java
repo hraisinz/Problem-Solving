f14ba4925d29d99349edc08552cd17f59b3e087f
hraisinz <hraisinz@gmail.com>
2019-08-27 13:43:21 +0530

    Calculate the power of given number using Recursion

---
+++ src/org/raisinz/PowerRecursion.java
@@ -0,0 +1,16 @@
+package org.raisinz;
+
+public class PowerRecursion {
+
+    public int getPower(int x,int n){
+        if(n != 0){
+            return (x * getPower(x,n-1));
+        }
+        else return 1;
+    }
+    public static void main(String[] args) {
+
+        PowerRecursion powerRecursion = new PowerRecursion();
+        System.out.println(powerRecursion.getPower(5,3));
+    }
+}
