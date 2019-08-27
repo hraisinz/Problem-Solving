201a19fcfdbc5df84107d99198d07413f2209082
hraisinz <hraisinz@gmail.com>
2019-08-27 19:52:46 +0530

    Calculate the power of given number using Recursion(Time Complexity)

---
+++ src/org/raisinz/PowerRecursionTimeComplexity.java
@@ -0,0 +1,18 @@
+package org.raisinz;
+
+public class PowerRecursionTimeComplexity {
+
+    public int getPower(int x,int n){
+        if(n == 0) return 1;
+        else if(n%2 == 0){
+            int y=getPower(x,n/2);
+            return y*y;
+        }
+        else return x*getPower(x,n-1);
+    }
+
+    public static void main(String[] args) {
+        PowerRecursionTimeComplexity powerRecursionTimeComplexity = new PowerRecursionTimeComplexity();
+        System.out.println(powerRecursionTimeComplexity.getPower(2,10));
+    }
+}
