f7c24d918f4b0c4d6469ebc65d251b7f866df346
hraisinz <hraisinz@gmail.com>
2019-08-28 16:08:22 +0530

    Inserting a Node in Linkedlist at the Nth position

--- src/org/raisinz/LinkedListNthPosition.java
+++ src/org/raisinz/LinkedListNthPosition.java
@@ -83,7 +83,6 @@ public class LinkedListNthPosition {
         }
     }
 
-
     public static void main(String[] args) {
 
         LinkedListNthPosition linkedListNthPosition = new LinkedListNthPosition();
