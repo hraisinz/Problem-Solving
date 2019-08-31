9bac2ce3b630e8f77a2daf4029e30bbc2e233e47
hraisinz <hraisinz@gmail.com>
2019-08-31 12:44:19 +0530

    Moving last node to front in a Linkedlist

---
+++ src/org/raisinz/LinkedlistLastToFront.java
@@ -0,0 +1,74 @@
+package org.raisinz;
+
+public class LinkedlistLastToFront {
+
+    private Node head;
+    private Node tail;
+
+    private class Node{
+        private int data;
+        private Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next=null;
+        }
+
+        private int getData() {
+            return data;
+        }
+
+        private Node getNext() {
+            return next;
+        }
+    }
+
+    public void inserNodeAtEnd(int data){
+        Node node = new Node(data);
+        if(head == null) {
+            head=node;
+            tail=node;
+        }
+        else{
+            tail.next=node;
+            tail=node;
+        }
+    }
+
+    public void moveToFront(){
+        if(head == null || head.next==null) return;
+        Node secondLast=null;
+        Node last=head;
+        while (last.next != null){
+            secondLast=last;
+            last=last.next;
+        }
+        secondLast.next=null;
+        last.next=head;
+        head=last;
+    }
+
+    public void getList(){
+        Node temp=head;
+        if(head == null){
+            System.out.println("List is Empty");
+        }
+        while (temp != null){
+            System.out.println(temp.getData());
+            temp=temp.getNext();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LinkedlistLastToFront linkedlistLastToFront = new LinkedlistLastToFront();
+        linkedlistLastToFront.inserNodeAtEnd(1);
+        linkedlistLastToFront.inserNodeAtEnd(2);
+        linkedlistLastToFront.inserNodeAtEnd(3);
+        linkedlistLastToFront.inserNodeAtEnd(4);
+        linkedlistLastToFront.inserNodeAtEnd(5);
+        linkedlistLastToFront.moveToFront();
+        linkedlistLastToFront.getList();
+
+    }
+}
