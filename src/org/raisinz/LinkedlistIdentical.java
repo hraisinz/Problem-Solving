81f0bcf5613eaab4697a1ff0069469bc5dad6a67
hraisinz <hraisinz@gmail.com>
2019-08-31 14:46:51 +0530

    Checking two Linkedlist are identical

---
+++ src/org/raisinz/LinkedlistIdentical.java
@@ -0,0 +1,63 @@
+package org.raisinz;
+
+public class LinkedlistIdentical {
+
+    private Node head;
+    private Node tail;
+
+    private class Node{
+        private int data;
+        private Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next=null;
+        }
+
+        private int getData() {
+            return data;
+        }
+
+        private Node getNext() {
+            return next;
+        }
+    }
+
+    public void inserNodeAtEnd(int data){
+        Node node = new Node(data);
+        if(head == null) {
+            head=node;
+            tail=node;
+        }
+        else{
+            tail.next=node;
+            tail=node;
+        }
+    }
+
+    public boolean checkIdentical(LinkedlistIdentical node){
+        Node first=head;
+        Node second=node.head;
+
+        while (first != null && second != null){
+            if(first.getData() != second.getData()) return false;
+            first=first.getNext();
+            second=second.getNext();
+        }
+
+        return (first == null && second == null);
+    }
+
+    public static void main(String[] args) {
+
+        LinkedlistIdentical linkedlistIdentical1 = new LinkedlistIdentical();
+        LinkedlistIdentical linkedlistIdentical2 = new LinkedlistIdentical();
+        linkedlistIdentical1.inserNodeAtEnd(11);
+        linkedlistIdentical1.inserNodeAtEnd(12);
+        linkedlistIdentical1.inserNodeAtEnd(13);
+        linkedlistIdentical2.inserNodeAtEnd(11);
+        linkedlistIdentical2.inserNodeAtEnd(12);
+        linkedlistIdentical2.inserNodeAtEnd(13);
+        System.out.println(linkedlistIdentical1.checkIdentical(linkedlistIdentical2));
+    }
+}
