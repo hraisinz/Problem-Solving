da35c7fd489f5957827f80ce03ad87ab62cf8e19
hraisinz <hraisinz@gmail.com>
2019-09-01 23:29:20 +0530

    Find second largest element of an array

---
+++ src/org/raisinz/FindSecondLargestElement.java
@@ -0,0 +1,28 @@
+package org.raisinz;
+
+public class FindSecondLargestElement {
+
+    public int getSecondLarge(int[] arr){
+
+        int size=arr.length;
+        int temp;
+        for (int i = 0; i <size ; i++) {
+            for (int j = i+1; j < size; j++) {
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        return arr[size-2];
+    }
+    public static void main(String[] args) {
+
+        int[] values={10,2,85,55,6,80,20};
+        FindSecondLargestElement findSecondLargestElement = new FindSecondLargestElement();
+        System.out.println(findSecondLargestElement.getSecondLarge(values));
+
+
+    }
+}
