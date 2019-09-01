d88a9f2a6456896ee0673fad79a4bad83baf4f6e
hraisinz <hraisinz@gmail.com>
2019-09-01 23:14:28 +0530

    Find largest and smallest element of an array

--- src/org/raisinz/FindMax.java
+++ src/org/raisinz/FindMax.java
@@ -9,12 +9,24 @@ public class FindMax {
                 max=arr[i];
             }
         }
-        System.out.println("Max Value : "+max);
+        System.out.println("Largest Element is : "+max);
+    }
+
+
+    public void getMin(int[] arr){
+        int min=arr[0];
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println("Smallest Element is : "+min);
     }
     public static void main(String[] args) {
 
-        int[] values={10,45,114,65,75,10};
+        int[] values={10,45,114,65,75,5};
         FindMax findMax = new FindMax();
         findMax.getMax(values);
+        findMax.getMin(values);
     }
 }
