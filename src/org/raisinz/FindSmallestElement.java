c62f6f31f45c832297056a894c25a17fc2ef985d
hraisinz <hraisinz@gmail.com>
2019-09-02 22:37:34 +0530

    Find Smallest element of an array

---
+++ src/org/raisinz/FindSmallestElement.java
@@ -0,0 +1,26 @@
+package org.raisinz;
+
+public class FindSmallestElement {
+
+    public int getSecondSmallest(int[] arr){
+
+        int size=arr.length;
+        int temp;
+        for (int i = 0; i <size ; i++) {
+            for (int j = i+1; j < size; j++) {
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        return arr[0];
+    }
+
+    public static void main(String[] args) {
+        int[] values={10,2,85,55,6,80,20};
+        FindSmallestElement findSmallestElement = new FindSmallestElement();
+        System.out.println(findSmallestElement.getSecondSmallest(values));
+    }
+}
