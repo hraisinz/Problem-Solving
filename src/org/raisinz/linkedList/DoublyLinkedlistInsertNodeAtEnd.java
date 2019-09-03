3575f72589128241e11c374f62eade9fbd2c21ae
hraisinz <hraisinz@gmail.com>
2019-09-03 16:17:39 +0530

    Inserting node at End in Doubly linkedlist

---
+++ src/org/raisinz/linkedList/DoublyLinkedlistInsertNodeAtEnd.java
@@ -0,0 +1,69 @@
+package org.raisinz.linkedList;
+
+public class DoublyLinkedlistInsertNodeAtEnd {
+
+    private Node head;
+    private Node tail;
+
+    private class Node{
+        private int data;
+        private Node next;
+        private Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next=null;
+            this.prev=null;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+    }
+
+    public void insertAtEnd(int data){
+        Node node = new Node(data);
+        if(head == null){
+            head=node;
+            tail=node;
+            return;
+        }
+        tail.next=node;
+        node.prev=tail;
+        tail=node;
+        tail.next=null;
+
+    }
+
+    public void getList(){
+        if(head == null){
+            System.out.println("List is Empty");
+            return;
+        }
+        Node temp=head;
+        while (temp != null){
+            System.out.println(temp.getData());
+            temp=temp.getNext();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DoublyLinkedlistInsertNodeAtEnd doublyLinkedlistInsertNodeAtEnd = new DoublyLinkedlistInsertNodeAtEnd();
+        doublyLinkedlistInsertNodeAtEnd.insertAtEnd(10);
+        doublyLinkedlistInsertNodeAtEnd.insertAtEnd(20);
+        doublyLinkedlistInsertNodeAtEnd.insertAtEnd(30);
+        doublyLinkedlistInsertNodeAtEnd.insertAtEnd(40);
+        doublyLinkedlistInsertNodeAtEnd.insertAtEnd(50);
+        doublyLinkedlistInsertNodeAtEnd.insertAtEnd(60);
+        doublyLinkedlistInsertNodeAtEnd.getList();
+    }
+}
