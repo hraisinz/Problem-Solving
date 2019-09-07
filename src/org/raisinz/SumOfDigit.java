5577621088be176b3af4966255292e8ee70c79c9
hraisinz <hraisinz@gmail.com>
2019-09-07 23:10:25 +0530

    Sum of digit of number using recursion

---
+++ src/org/raisinz/SumOfDigit.java
@@ -0,0 +1,13 @@
+package org.raisinz;
+
+public class SumOfDigit {
+
+    public int getSumOfDigit(int number){
+        if(number == 0) return 0;
+        return (number%10 + getSumOfDigit(number/10));
+    }
+    public static void main(String[] args) {
+        SumOfDigit sumOfDigit = new SumOfDigit();
+        System.out.println("Sum is : "+sumOfDigit.getSumOfDigit(123456));
+    }
+}
