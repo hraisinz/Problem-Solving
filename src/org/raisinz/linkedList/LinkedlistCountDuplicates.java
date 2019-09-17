54fdedfbfd3a3c1384a4c22a1b39e83443e007d3
hraisinz <hraisinz@gmail.com>
2019-09-17 23:48:45 +0530

    Sum of nonduplicates node in a Linkedlist

--- src/org/raisinz/linkedList/LinkedlistCountDuplicates.java
+++ src/org/raisinz/linkedList/LinkedlistCountDuplicates.java
@@ -37,18 +37,42 @@ public class LinkedlistCountDuplicates {
 
     public int countDuplicates(){
         int count=0;
-        while (head.getNext() != null){
-            Node t=head.getNext();
+        Node t1=head;
+        while (t1.getNext() != null){
+            Node t=t1.getNext();
             while (t.getNext() != null){
-                if(head.getData() == t.getData()){
+                if(t1.getData() == t.getData()){
                     count++;
                     break;
                 }
                 t=t.getNext();
             }
-            head=head.getNext();
+            t1=t1.getNext();
         }
         return count;
+    }
+
+    public int sumOfNonDuplicateNode(){
+        Node t1=head;
+        Node t2;
+        int sum=0;
+        while (t1 != null){
+            t2=head;
+            boolean flag=false;
+            while (t2 != null){
+                if(t1!=t2 && t1.getData() == t2.getData()){
+                    flag=true;
+                    break;
+                }
+                t2=t2.getNext();
+            }
+
+            if(!flag){
+                sum=sum+t1.getData();
+            }
+            t1=t1.getNext();
+        }
+        return sum;
     }
     public static void main(String[] args) {
 
@@ -56,9 +80,10 @@ public class LinkedlistCountDuplicates {
         linkedlistCountDuplicates.inserNodeAtEnd(10);
         linkedlistCountDuplicates.inserNodeAtEnd(20);
         linkedlistCountDuplicates.inserNodeAtEnd(10);
-        linkedlistCountDuplicates.inserNodeAtEnd(20);
+        linkedlistCountDuplicates.inserNodeAtEnd(30);
         linkedlistCountDuplicates.inserNodeAtEnd(40);
         linkedlistCountDuplicates.inserNodeAtEnd(20);
         System.out.println(linkedlistCountDuplicates.countDuplicates());
+        System.out.println(linkedlistCountDuplicates.sumOfNonDuplicateNode());
     }
 }
