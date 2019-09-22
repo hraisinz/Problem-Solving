89616dffa3a2f74c3f0aaf475c53f23233a92906
hraisinz <hraisinz@gmail.com>
2019-09-22 16:27:41 +0530

    Refactoring

--- src/org/raisinz/sortings/MergeSort.java
+++ src/org/raisinz/sortings/MergeSort.java
@@ -2,6 +2,7 @@ package org.raisinz.sortings;
 
 public class MergeSort {
 
+    private int[] c;
 
     public void MergeArray(int[] arr,int[] brr){
         int i=0;
@@ -9,8 +10,7 @@ public class MergeSort {
         int k=0;
         int m=arr.length;
         int n=brr.length;
-        int length=m+n;
-        int[] c=new int[length];
+        c=new int[m+n];
         while (i < m && j < n){
             if(arr[i] < brr[j]) c[k++]=arr[i++];
             else c[k++]=brr[j++];
@@ -23,15 +23,18 @@ public class MergeSort {
             c[k]=brr[j];
             k++;
         }
+    }
+
+    public void getElements(){
         for (int l = 0; l <c.length ; l++) {
             System.out.println(c[l]);
         }
     }
     public static void main(String[] args){
-
         int[] a={2,8,15,18};
         int[] b={5,9,12,17};
         MergeSort mergeSort = new MergeSort();
         mergeSort.MergeArray(a,b);
+        mergeSort.getElements();
     }
 }
