9957d13b0eaaba4c93413f3ebf9b56aa3aaa3321
hraisinz <hraisinz@gmail.com>
2019-09-24 22:56:29 +0530

    Refactoring

--- src/org/raisinz/arrays/FindMax.java
+++ src/org/raisinz/arrays/FindMax.java
@@ -14,9 +14,7 @@ public class FindMax {
     public void getMin(int[] arr){
         int min=arr[0];
         for (int i = 0; i <arr.length ; i++) {
-            if(arr[i]<min){
-                min=arr[i];
-            }
+            if(arr[i]<min) min=arr[i];
         }
         System.out.println("Smallest Element is : "+min);
     }
