2d42df753c282e6f8e3655ceb217088ae2151ec5
hraisinz <hraisinz@gmail.com>
2019-09-24 22:58:04 +0530

    Refactoring

--- src/org/raisinz/arrays/PowerRecursion.java
+++ src/org/raisinz/arrays/PowerRecursion.java
@@ -3,13 +3,10 @@ package org.raisinz.arrays;
 public class PowerRecursion {
 
     public int getPower(int x,int n){
-        if(n != 0){
-            return (x * getPower(x,n-1));
-        }
+        if(n != 0) return (x * getPower(x,n-1));
         else return 1;
     }
     public static void main(String[] args) {
-
         PowerRecursion powerRecursion = new PowerRecursion();
         System.out.println(powerRecursion.getPower(5,3));
     }
