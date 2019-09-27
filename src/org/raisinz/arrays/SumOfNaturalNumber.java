7520c3706a707b4d2dbe754470f044dcdde386f5
hraisinz <hraisinz@gmail.com>
2019-09-27 15:49:50 +0530

    Sum of natural number using Recursion

--- src/org/raisinz/arrays/SumOfNaturalNumber.java
+++ src/org/raisinz/arrays/SumOfNaturalNumber.java
@@ -5,7 +5,7 @@ public class SumOfNaturalNumber {
     //Using Iterative method
     int getSum(int n){
         int s=0;
-        for (int i = 0; i <= n ; i++) {
+        for (int i = 1; i <= n ; i++) {
             s=s+i;
         }
         return s;
