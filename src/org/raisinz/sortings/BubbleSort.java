c8275d88107141dc03dd422ca292e99e81d79cf4
hraisinz <hraisinz@gmail.com>
2019-10-05 17:26:23 +0530

    Refactoring

--- src/org/raisinz/sortings/BubbleSort.java
+++ src/org/raisinz/sortings/BubbleSort.java
@@ -9,9 +9,6 @@ public class BubbleSort {
         for (int i = 0; i <size ; i++) {
             for (int j = 1; j <(size-i) ; j++) {
                 if(arr[j-1] > arr[j]){
-//                    temp=arr[j-1];
-//                    arr[j-1]=arr[j];
-//                    arr[j]=temp;
                     swap(arr,j-1,j);
                     flag=false;
                 }
