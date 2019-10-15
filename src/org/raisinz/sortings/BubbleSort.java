e541cc54bdcd8b1b2e98e2e97c9b5a4222b066fd
hraisinz <hraisinz@gmail.com>
2019-10-15 11:47:20 +0530

    Refactoring

--- src/org/raisinz/sortings/BubbleSort.java
+++ src/org/raisinz/sortings/BubbleSort.java
@@ -20,6 +20,21 @@ public class BubbleSort {
         }
     }
 
+
+    //Alternative
+    public void bubblesort(int[] arr){
+        int size=arr.length;
+        boolean flag=true;
+        for (int i = 0; i < size-1 ; i++) {
+            for (int j = 0; j < size-1-i ; j++) {
+                if(arr[j]>arr[j+1]){
+                    swap(arr,j,j+1);
+                    flag=false;
+                }
+            }
+        }
+    }
+
     private void swap(int [] values,int v1,int v2){
         int temp=values[v1];
         values[v1]=values[v2];
@@ -40,5 +55,7 @@ public class BubbleSort {
         bubbleSort.getElements(values);
         int[] values1={2,3,10,15,25,56};
         bubbleSort.bubbleSortAlgo(values1);
+        bubbleSort.bubblesort(values);
+        bubbleSort.getElements(values);
     }
 }
