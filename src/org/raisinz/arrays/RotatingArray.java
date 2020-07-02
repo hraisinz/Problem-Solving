9944877cfffbcb8a296a9c0c7ac4bd4384128228
hraisinz <hraisinz@gmail.com>
2020-07-02 09:34:25 +0530

    Refactoring

--- src/org/raisinz/arrays/RotatingArray.java
+++ src/org/raisinz/arrays/RotatingArray.java
@@ -25,6 +25,7 @@ public class RotatingArray {
     public static void main(String[] args) {
 
         int[] arr={1,2,3,4,5,6,7,8,9};
+        System.out.println(arr.length);
         int rotation=3;
         RotatingArray rotatingArray = new RotatingArray();
         rotatingArray.leftRotation(arr,rotation);
